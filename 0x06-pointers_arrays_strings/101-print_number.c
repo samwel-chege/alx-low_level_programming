72283923cf2fcf7e9ff0d404ea2452476d334b05
samwel-chege <samwelchegeh09@gmail.com>
2022-03-24 16:22:34 +0300

    update files

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ */
+void print_number(int n)
 {
-  int n;
-  int a[5];
-  int *p;
+	unsigned int n1;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
+}rite your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
